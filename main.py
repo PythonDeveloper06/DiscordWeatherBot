927e05da03861626f60a1f697d7d9c8eacea1fa2
Pavel Lednev <lednevpasha@mail.ru>
2023-03-26 16:57:09 +0600

    remove_member

--- main.py
+++ main.py
@@ -30,6 +30,12 @@ async def on_member_join(member):
     await channel.send(f'Добро пожаловать {member.mention} в {member.guild.name}')
 
 
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(1089208954556522537)
+    await channel.send(f'Пока {member.mention}')
+
+
 @bot.command(name='weather')
 async def text(ctx, city: str):
     await ctx.send(city.capitalize())
