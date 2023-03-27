ce304121e26d67fd737c520b6a9f72a03d41b6c4
Pavel Lednev <lednevpasha@mail.ru>
2023-03-27 16:27:26 +0600

    add mixin and type values

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import discord
+from discord import Message
 from discord.ext import commands
 import logging
+import asyncio
 
 from conf import TOKEN
+from mixins import on_member
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
@@ -15,31 +18,28 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 
 
 @bot.event
-async def on_ready():
+async def on_ready() -> None:
     logger.info(f'{bot.user} has connected to Discord!')
     for guild in bot.guilds:
         logger.info(
             f'{bot.user} подключился к чату:\n'
-            f'{guild.name}(ID: {guild.id})'
+            f'{guild.name} (ID: {guild.id})'
         )
 
 
 @bot.event
-async def on_member_join(member):
-    channel = bot.get_channel(1089208954556522537)
-    await channel.send(f'Добро пожаловать {member.mention} в {member.guild.name}')
+async def on_member_join(member: discord.Member) -> None:
+    asyncio.create_task(on_member('Привет', bot, member))
 
 
 @bot.event
-async def on_member_remove(member):
-    channel = bot.get_channel(1089208954556522537)
-    await channel.send(f'Пока {member.mention}')
+async def on_member_remove(member: discord.Member) -> None:
+    asyncio.create_task(on_member('Пока', bot, member))
 
 
-@bot.command(name='weather')
-async def text(ctx, city: str):
-    await ctx.send(city.capitalize())
+@bot.command(name='weather', description="Get weather")
+async def text(ctx: Message, city: str) -> None:
+    await ctx.send(embed=discord.Embed(title=f'Your city {city.capitalize()}', description='Wait a one minute...'))
 
 
 bot.run(TOKEN)
-
---
+++ mixins.py
@@ -0,0 +1,11 @@
+from discord import Member, Message
+from discord.ext.commands import Bot
+from discord.channel import TextChannel
+
+
+async def on_member(word: str, bot: Bot, member: Member) -> Message:
+    for guild in bot.guilds:
+        if guild == member.guild:
+            for tc in guild.text_channels:
+                channel: TextChannel = bot.get_channel(tc.id)
+                return await channel.send(f'{word} {member.mention}')
